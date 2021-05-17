3b60fb1b5cbdd870b574ef4be970934334ce4acb
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-05-17 21:56:20 +0300

    de

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,20 +1,39 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import Toast from "react-bootstrap/Toast";
+import Badge from "react-bootstrap/Badge";
 
 function TodoList(props) {
   return (
-    <ul>
-      {props.list.map((item) => (
-        <li className={`complete-${item.complete.toString()}`} key={item._id}>
-          <span className="listColor" onClick={() => props.handleComplete(item._id)}>
-            {item.text}
-          </span>
-          <Button variant="danger" onClick={() => props.handleDelete(item._id)}>X</Button>
-          <Button onClick={() => props.handleUpdate(item._id)}>Edit</Button>
-        </li>
-      ))}
-    </ul>
+    <section className="bigS">
+      <Toast className="oot">
+        {props.list.map((item) => (
+          <div className="list--boot" key={item._id + "_"}>
+            <Toast.Header>
+              <Badge
+                className="badgeN"
+                variant={item.complete ? "danger" : "success"}
+                onClick={() => props.handleComplete(item._id)}
+              >
+                {item.complete ? "Complete" : "pending"}
+              </Badge>
+              <strong>{item.assignee}</strong>
+            </Toast.Header>
+            <Toast.Body>
+              <span className="listColor" onClick={() => props.handleComplete(item._id)}>
+                {item.text}
+              </span>
+            </Toast.Body>
+            <Toast.Body>
+              <span onClick={() => props.handleComplete(item._id)}>
+                Difficulty : {item.difficulty}
+              </span>
+            </Toast.Body>
+          </div>
+        ))}
+      </Toast>
+    </section>
   );
 }
 
+// <Button variant="danger" onClick={() => props.handleDelete(item._id)}>X</Button>
 export default TodoList;
